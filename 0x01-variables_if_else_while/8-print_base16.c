8660d086186812cf4acae3ee9111e2f7f9cbbed0
eyoba65 <eyobadugnafanta@gmail.com>
2022-04-11 13:27:36 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int i;
+	char letter;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar(10);
+	return (0);
+}
