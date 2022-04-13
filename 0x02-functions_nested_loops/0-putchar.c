18d8571d6e713deeb19dcf2ca8ea5e8dbe31a351
eyoba65 <eyobadugnafanta@gmail.com>
2022-04-13 16:05:14 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print the string _putchar as a message
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count;
+	int size;
+
+	size = sizeof(str) / sizeof(int);
+	for (count = 0; count < size; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar(10);
+	return (0);
+}
